e1181a876fb2864c648b170bfba0c22a69f37731
ashu-bhatta <ashu.bhatta2005@gmail.com>
2023-06-13 12:43:35 +0530

    First commit

---
+++ page.js
@@ -0,0 +1,12 @@
+const container = document.getElementById('js_container');
+const highlight = document.getElementById('progress-bar');
+var containerHeight;
+
+window.onscroll = function(){
+    containerHeight = container.offsetHeight - window.innerHeight;
+    containerPos= container.getBoundingClientRect();
+    diff=containerHeight+containerPos.top;
+    progressPercentage=diff/containerHeight*100;
+    cssWidth=Math.floor(100-progressPercentage);
+    highlight.style.width=cssWidth+"%";
+}
